d937cf87ae361fa482232d56ae17883b2fbdad86
Armin <armin.salihovic@live.com>
2020-12-08 08:33:27 +0100

    update

--- send-email.php
+++ send-email.php
@@ -4,18 +4,26 @@ function checkRequiredField($value) {
     return isset($value) && !empty($value);
 }
 
-if($_POST['email-message'] == null) {
-    if(checkRequiredField($_POST['name']) && checkRequiredField($_POST['email']) && checkRequiredField($_POST['subject']) && checkRequiredField($_POST['message'])) {
-        $email_to = "info@mimo.ba";
-        $name = $_POST['name'];
-        $email_from = $_POST['email'];
-        $subject = $_POST['subject'];
-        $message = $_POST['message'];
+$to      = 'info@mimo.ba';
+$subject = 'First email from Mimo!';
+$message = 'Hey just checking in !';
+$headers = 'From: armin.salihovic@live.com' . "\r\n" .
+    'Reply-To: armin.salihovic@live.com' . "\r\n";
 
-        //$header = "From: {$name} <{$email_from}>";
-        $header = "From: {$email_from}";
+mail($to, $subject, $message, $headers);
 
-        mail($email_to, $subject, $message, $header);
-    }
-}
+//if($_POST['email-message'] == null) {
+//    if(checkRequiredField($_POST['name']) && checkRequiredField($_POST['email']) && checkRequiredField($_POST['subject']) && checkRequiredField($_POST['message'])) {
+//        $email_to = "info@mimo.ba";
+//        $name = $_POST['name'];
+//        $email_from = $_POST['email'];
+//        $subject = $_POST['subject'];
+//        $message = $_POST['message'];
+//
+//        //$header = "From: {$name} <{$email_from}>";
+//        $header = "From: {$email_from}";
+//
+//        mail($email_to, $subject, $message, $header);
+//    }
+//}
 
