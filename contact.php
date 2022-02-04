a9bc758e44d7a60d17f109ec0f3f9423fa67fcd1
Armin Salihovic <armin.salihovic@live.com>
2022-02-04 20:02:15 +0100

    update copyright year

--- contact.php
+++ contact.php
@@ -229,7 +229,7 @@ if (isset($_POST['email-message'])) {
     </div>
 </main>
 
-<footer class="mt-auto d-flex justify-content-center"><p>&copy; 2020-2021 Emir Salihovic Mimo</p></footer>
+<footer class="mt-auto d-flex justify-content-center"><p>&copy; 2020-2022 Emir Salihovic Mimo</p></footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
