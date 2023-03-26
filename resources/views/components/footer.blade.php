e86cff6e889dfd5c58b69fd784b538d9485e6c43
Armin Salihovic <armin.salihovic@live.com>
2023-03-26 19:53:41 +0200

    Add letter accent in the footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,1 @@
-<footer class="mt-auto d-flex justify-content-center"><p>&copy; 2020-{{ now()->year }} Emir Salihovic Mimo</p></footer>
+<footer class="mt-auto d-flex justify-content-center"><p>&copy; 2020-{{ now()->year }} Emir Salihović Mimo</p></footer>
