b059edd51e7f010b43fb05b3d740a274f106bd20
Armin Salihovic <armin.salihovic@live.com>
2023-03-26 16:53:12 +0200

    Update meta title on Art 2020s

--- resources/views/art/2020.blade.php
+++ resources/views/art/2020.blade.php
@@ -1,6 +1,6 @@
 @php
     $meta = [
-       "title" => "Art | Emir Salihović Mimo",
+       "title" => "Art 2020s | Emir Salihović Mimo",
        "description" => "A selection of abstract paintings by Emir Salihović Mimo, hand-picked by the artist himself.",
        "thumbnail" => "https://cdn.mimo.ba/img/og-art-thumbnail.jpg",
     ];
