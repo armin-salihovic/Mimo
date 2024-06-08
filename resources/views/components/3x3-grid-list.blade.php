f9b3af45ef25be356ae73c862aff640edd001b2c
Armin Salihović <armin.salihovic@live.com>
2024-06-08 21:56:51 +0200

    lazy loading grid images

--- resources/views/components/3x3-grid-list.blade.php
+++ resources/views/components/3x3-grid-list.blade.php
@@ -9,8 +9,49 @@
     for (count($items) - $remainder - 1; $i < count($items); $i++) {
         $remainderCollection[] = $items[$i];
     }
+
+//    temporary
+    $collection = $items;
 @endphp
 
+@push('styles')
+
+    <style>
+        .blur-div {
+            background-size: cover;
+            background-position: center;
+        }
+
+        .blur-div.loaded > img {
+            opacity: 1;
+        }
+
+        .blur-div > img {
+            opacity: 0;
+            transition: opacity 200ms ease-in-out;
+        }
+    </style>
+@endpush
+
+@push('scripts')
+    <script>
+        const blurDivs = document.querySelectorAll('.blur-div')
+        blurDivs.forEach(div => {
+            const img = div.querySelector('img')
+
+            function loaded() {
+                div.classList.add('loaded')
+            }
+
+            if (img.complete) {
+                loaded();
+            } else {
+                img.addEventListener('load', loaded)
+            }
+        })
+    </script>
+@endpush
+<x-scroll-to-top />
 <div class="py-24 px-4 sm:px-6">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-24 {{ $remainder !== 0 ? 'mb-24' : ''}}">
         @foreach($collection as $item)
@@ -22,61 +63,77 @@
                             <h3 class="text-lg md:text-lg lg:text-xl text-gray-700 text-center">{{ $item->{$subtitleName} }}</h3>
                         </div>
                         <div class="w-full md:hidden shadow-lg">
-                            <img src="{{ $item->image($thumbnailRole, 'mobile') }}"
-                                 alt="{{ $item->imageAltText($thumbnailRole) }}">
+                            <div
+                                class="blur-div"
+                                style="background-image: url({{ $item->lowQualityImagePlaceholder($thumbnailRole, 'mobile') }})"
+                            >
+                                <img src="{{ $item->image($thumbnailRole, 'mobile') }}"
+                                     alt="{{ $item->imageAltText($thumbnailRole) }}"
+                                     style="aspect-ratio: 1/1;"
+                                     loading="lazy"
+                                >
+                            </div>
                         </div>
                         <div class="hidden md:block shadow-lg">
-                            <img src="{{ $item->image($thumbnailRole) }}"
-                                 alt="{{ $item->imageAltText($thumbnailRole) }}">
+                            <div
+                                class="blur-div"
+                                style="background-image: url({{ $item->lowQualityImagePlaceholder($thumbnailRole) }})"
+                            >
+                                <img src="{{ $item->image($thumbnailRole) }}"
+                                     alt="{{ $item->imageAltText($thumbnailRole) }}"
+                                     style="aspect-ratio: 4/5;"
+                                     loading="lazy"
+                                >
+                            </div>
                         </div>
                     </div>
                 </a>
             </div>
         @endforeach
     </div>
-    @if ($remainder === 1)
-        <div class="grid grid-cols-1 gap-x-12 gap-y-24">
-            <div class="pb-16 md:pb-8">
-                <a href="{{ route($route, $remainderCollection[0]->slug) }}">
-                    <div class="flex flex-col-reverse gap-14">
-                        <div class="w-full">
-                            <h2 class="text-lg font-bold md:text-lg lg:text-xl hover:underline text-center">{{ $remainderCollection[0]->title }}</h2>
-                            <h3 class="text-lg md:text-lg lg:text-xl text-gray-700 text-center">{{ $remainderCollection[0]->{$subtitleName} }}</h3>
-                        </div>
-                        <div class="w-full md:hidden shadow-lg">
-                            <img src="{{ $remainderCollection[0]->image($thumbnailRole, 'mobile') }}"
-                                 alt="{{ $remainderCollection[0]->imageAltText($thumbnailRole) }}">
-                        </div>
-                        <div class="hidden md:block shadow-lg">
-                            <img class="w-full" src="{{ $remainderCollection[0]->image($thumbnailRole, 'wide') }}"
-                                 alt="{{ $remainderCollection[0]->imageAltText($thumbnailRole) }}">
-                        </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-    @elseif($remainder === 2)
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-12 gap-y-24">
-            @foreach($remainderCollection as $item)
-                <div class="pb-16 md:pb-8">
-                    <a href="{{ route($route, $item->slug) }}">
-                        <div class="flex flex-col-reverse gap-14">
-                            <div class="w-full">
-                                <h2 class="text-lg font-bold md:text-lg lg:text-xl hover:underline text-center">{{ $item->title }}</h2>
-                                <h3 class="text-lg md:text-lg lg:text-xl text-gray-700 text-center">{{ $item->{$subtitleName} }}</h3>
-                            </div>
-                            <div class="w-full md:hidden shadow-lg">
-                                <img src="{{ $item->image($thumbnailRole, 'mobile') }}"
-                                     alt="{{ $item->imageAltText($thumbnailRole) }}">
-                            </div>
-                            <div class="hidden md:block shadow-lg">
-                                <img src="{{ $item->image($thumbnailRole, 'mobile') }}"
-                                     alt="{{ $item->imageAltText($thumbnailRole) }}">
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            @endforeach
-        </div>
-    @endif
+{{--    @if ($remainder === 1)--}}
+{{--        <div class="grid grid-cols-1 gap-x-12 gap-y-24">--}}
+{{--            <div class="pb-16 md:pb-8">--}}
+{{--                <a href="{{ route($route, $remainderCollection[0]->slug) }}">--}}
+{{--                    <div class="flex flex-col-reverse gap-14">--}}
+{{--                        <div class="w-full">--}}
+{{--                            <h2 class="text-lg font-bold md:text-lg lg:text-xl hover:underline text-center">{{ $remainderCollection[0]->title }}</h2>--}}
+{{--                            <h3 class="text-lg md:text-lg lg:text-xl text-gray-700 text-center">{{ $remainderCollection[0]->{$subtitleName} }}</h3>--}}
+{{--                        </div>--}}
+{{--                        <div class="w-full md:hidden shadow-lg">--}}
+{{--                            <img src="{{ $remainderCollection[0]->image($thumbnailRole, 'mobile') }}"--}}
+{{--                                 alt="{{ $remainderCollection[0]->imageAltText($thumbnailRole) }}">--}}
+{{--                        </div>--}}
+{{--                        <div class="hidden md:block shadow-lg">--}}
+{{--                            <img class="w-full" src="{{ $remainderCollection[0]->image($thumbnailRole, 'wide') }}"--}}
+{{--                                 alt="{{ $remainderCollection[0]->imageAltText($thumbnailRole) }}">--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    @elseif($remainder === 2)--}}
+{{--        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-12 gap-y-24">--}}
+{{--            @foreach($remainderCollection as $item)--}}
+{{--                <div class="pb-16 md:pb-8">--}}
+{{--                    <a href="{{ route($route, $item->slug) }}">--}}
+{{--                        <div class="flex flex-col-reverse gap-14">--}}
+{{--                            <div class="w-full">--}}
+{{--                                <h2 class="text-lg font-bold md:text-lg lg:text-xl hover:underline text-center">{{ $item->title }}</h2>--}}
+{{--                                <h3 class="text-lg md:text-lg lg:text-xl text-gray-700 text-center">{{ $item->{$subtitleName} }}</h3>--}}
+{{--                            </div>--}}
+{{--                            <div class="w-full md:hidden shadow-lg">--}}
+{{--                                <img src="{{ $item->image($thumbnailRole, 'mobile') }}"--}}
+{{--                                     alt="{{ $item->imageAltText($thumbnailRole) }}">--}}
+{{--                            </div>--}}
+{{--                            <div class="hidden md:block shadow-lg">--}}
+{{--                                <img src="{{ $item->image($thumbnailRole, 'mobile') }}"--}}
+{{--                                     alt="{{ $item->imageAltText($thumbnailRole) }}">--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </a>--}}
+{{--                </div>--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--    @endif--}}
 </div>
