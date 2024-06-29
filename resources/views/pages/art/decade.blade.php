117763298f67f579851a06994afeddbfb8c6302f
Armin Salihović <armin.salihovic@live.com>
2024-06-29 22:25:52 +0200

    refactor art decade view

--- resources/views/pages/art/decade.blade.php
+++ resources/views/pages/art/decade.blade.php
@@ -1,47 +1,49 @@
 @php
     $seen = [];
+    $filteredArtsByYears = [];
+    foreach($arts as $art) {
+        if (!array_key_exists($art->year, $filteredArtsByYears)) {
+            $filteredArtsByYears[$art->year] = [];
+        }
+
+        $filteredArtsByYears[$art->year][] = $art;
+    }
+    krsort($filteredArtsByYears);
 @endphp
 <x-art-layout :arts="$arts" :art="$art ?? null" :art-links="$artLinks" :meta="$meta">
     <x-slot name="gallery">
         <div id="gallery">
-            @foreach($arts as $art)
-                @if(!in_array($art->year, $seen))
-                    @php
-                        $seen[] = $art->year;
-                    @endphp
-                    <div class="flex flex-col mb-24 px-6">
-                        <x-gallery-title :title="$art->year"/>
+            @foreach($filteredArtsByYears as $year => $filteredArtsByYear)
+                <div class="flex flex-col mb-24 px-6">
+                    <x-gallery-title :title="$year"/>
 
-                        <div class="w-full">
-                            <div class=" h-full">
-                                <div class="columns-1 md:columns-2 lg:columns-3">
-                                    @foreach($arts as $art2)
-                                        @if($art->year === $art2->year)
-                                            <x-lazy-loading-wrapper :lqip="$art2->lowQualityImagePlaceholder('image', 'free')">
-                                                <a class="gallery-item"
-                                                   href="{{ $art2->image('image', 'free') }}"
-                                                   title="{{ $art2->generateTitle() }}"
-                                                   data-year="{{ $art2->year }}"
-                                                   data-width="{{ $art2->width }}"
-                                                   data-height="{{ $art2->height }}"
-                                                   data-status="{{ $art2->status }}"
-                                                   data-technique="{{ $art2->technique ?? 'No Information' }}"
-                                                   data-sn="{{ $art2->serial_number }}"
-                                                >
-                                                    <x-image-art class="mb-4"
-                                                                 :alt="$art2->generateTitle()"
-                                                                 :img="$art2->image('image', 'free')"
-                                                                 :aspect-ratio="$art2->getAspectRatioFormatted('image', 'free')"
-                                                    />
-                                                </a>
-                                            </x-lazy-loading-wrapper>
-                                        @endif
-                                    @endforeach
-                                </div>
+                    <div class="w-full">
+                        <div class=" h-full">
+                            <div class="columns-1 md:columns-2 lg:columns-3">
+                                @foreach($filteredArtsByYear as $art)
+                                    <x-lazy-loading-wrapper :lqip="$art->lowQualityImagePlaceholder('image', 'free')">
+                                        <a class="gallery-item"
+                                           href="{{ $art->image('image', 'free') }}"
+                                           title="{{ $art->generateTitle() }}"
+                                           data-year="{{ $art->year }}"
+                                           data-width="{{ $art->width }}"
+                                           data-height="{{ $art->height }}"
+                                           data-status="{{ $art->status }}"
+                                           data-technique="{{ $art->technique ?? 'No Information' }}"
+                                           data-sn="{{ $art->serial_number }}"
+                                        >
+                                            <x-image-art class="mb-4"
+                                                         :alt="$art->generateTitle()"
+                                                         :img="$art->image('image', 'free')"
+                                                         :aspect-ratio="$art->getAspectRatioFormatted('image', 'free')"
+                                            />
+                                        </a>
+                                    </x-lazy-loading-wrapper>
+                                @endforeach
                             </div>
                         </div>
                     </div>
-                @endif
+                </div>
             @endforeach
         </div>
     </x-slot>
